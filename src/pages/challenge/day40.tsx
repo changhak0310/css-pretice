7240c875501e1266a97020722afc5d82b1c9ff3c
changhak0310 <dlckdgkr0320@naver.com>
2023-07-29 23:16:44 +0900

    Day40

---
+++ src/pages/challenge/day40.tsx
@@ -0,0 +1,35 @@
+/** @jsxImportSource @emotion/react */
+
+import styled from "@emotion/styled"
+import { css, keyframes } from "@emotion/react"
+import { useState } from 'react';
+
+
+
+const Frame = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 400px;
+    height: 400px;
+    margin-top: -200px;
+    margin-left: -200px;
+    border-radius: 2px;
+    box-shadow: 1px 2px 10px 0px rgba(0, 0, 0, 0.3);
+    background: #3FAF82;
+    color: #fff;
+`
+
+
+
+const Day2 = () => {
+
+
+    return(
+        <Frame>
+            
+        </Frame>
+    )
+}
+
+export default Day2
